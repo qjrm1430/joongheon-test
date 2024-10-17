59196425c48c9f10238233397629f30a0c467b09
hon2y_b22 <qjrm1430@naver.com>
2024-10-17 17:50:38 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 30.69 ms, Memory: 9.93 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.py
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.py
@@ -2,6 +2,7 @@
 # input  : 자연수 n
 # output : n을 x로 나눈 나머지가 1이 되도록 하는 가장 작은 자연수 x
 def solution(n):
-    for x in range(1, n+1):
+    for x in range(1, (n//2)+1):
         if n % x == 1:
             return x
+    return n-1
